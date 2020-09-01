e404df1ff634f9a700e923ba0500e580546a9a52
phanthienhoang95 <phanthienhoang95@gmail.com>
2020-09-01 09:54:47 +0700

    .

--- inclue/DBConnection.php
+++ inclue/DBConnection.php
@@ -7,7 +7,7 @@ class DBConnection
     private $username = "zhelxwftazmksc";
     private $password = "888a4c12cf1cb020658368757d4ea0475abd9de46776833421e99bd74d54edca";
     private $dbname = "d3sqf7vu1hi6m4";
-    private $charset = "utf8";
+    // private $charset = "utf8";
     private $port = "5432";
 
 
@@ -24,7 +24,7 @@ class DBConnection
 
     public function connection (){        
         try {
-            $dsn = "pgsql:host=".$this->servername.";port=".$this->port.";dbname=".$this->dbname.";charset=".$this->charset;
+            $dsn = "pgsql:host=".$this->servername.";port=".$this->port.";dbname=".$this->dbname;
             $pdo =  new PDO($dsn,$this->username,$this->password);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);  
             $pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES , false);   
