7d0751cd19e442e7f1a637f36c245916ce5682be
phanthienhoang95 <phanthienhoang95@gmail.com>
2020-09-01 08:29:27 +0700

    port

--- inclue/DBConnection.php
+++ inclue/DBConnection.php
@@ -8,14 +8,14 @@ class DBConnection
     private $password = "888a4c12cf1cb020658368757d4ea0475abd9de46776833421e99bd74d54edca";
     private $dbname = "d3sqf7vu1hi6m4";
     private $charset = "utf8";
-
+    private $port = "5432";
     public function __construct(){
         $this->con = $this->connection();
     }
 
     public function connection (){        
         try {
-            $dsn = "mysql:host=".$this->servername.";dbname=".$this->dbname .";charset=".$this->charset;
+            $dsn = "mysql:host=".$this->servername.";port=".$this->port.";dbname=".$this->dbname .";charset=".$this->charset;
             $pdo =  new PDO($dsn,$this->username,$this->password);
             $pdo->setAttribute(PDO::ATTR_ERRMODE , PDO::ERRMODE_EXCEPTION);
             $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);         
