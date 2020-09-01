4da80e4a695747ee7b5a93c65c7ed43b71f30332
phanthienhoang95 <phanthienhoang95@gmail.com>
2020-09-01 09:15:36 +0700

    .

--- inclue/DBConnection.php
+++ inclue/DBConnection.php
@@ -2,21 +2,14 @@
 
 class DBConnection
 {
-    // public $con;
-    // private $servername = "ec2-54-160-120-28.compute-1.amazonaws.com";
-    // private $username = "zhelxwftazmksc";
-    // private $password = "888a4c12cf1cb020658368757d4ea0475abd9de46776833421e99bd74d54edca";
-    // private $dbname = "d3sqf7vu1hi6m4";
-    // private $charset = "utf8";
-    private $port = "3306";
-
-
     public $con;
-    private $servername = "localhost";
-    private $username = "root";
-    private $password = "";
-    private $dbname = "coffee_shop_4.0";
+    private $servername = "ec2-54-160-120-28.compute-1.amazonaws.com";
+    private $username = "zhelxwftazmksc";
+    private $password = "888a4c12cf1cb020658368757d4ea0475abd9de46776833421e99bd74d54edca";
+    private $dbname = "d3sqf7vu1hi6m4";
     private $charset = "utf8";
+    private $port = "5432";
+
 
     public function __construct(){
         $this->con = $this->connection();
